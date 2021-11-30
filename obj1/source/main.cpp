a0d3451c8fa28aaa2f27c5e4b3b8d8dddc4b1c76
ubuntu <ubuntu@localhost.localdomain>
2021-11-30 15:09:43 +0800

    更改文件夹结构

---
+++ obj1/source/main.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: renyoutong
+	> Mail: justiceryt@163.com 
+	> Created Time: Tue 30 Nov 2021 10:36:00 AM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+/*
+ * 强制以小数，小数点后三位显示
+	cout << fixed;
+	cout << setprecision(3);
+
+	double doubleNum = 100.0 / 3.0;
+	cout << doubleNum * 1000000 << endl;
+*/
+	return 0;
+}
