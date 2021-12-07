3fa5acdcd6ec61813dcc5ca17db8b34b553677c3
ubuntu <ubuntu@localhost.localdomain>
2021-12-07 15:08:59 +0800

    增加了一些关于循环的小例子

--- obj1/source/main.cpp
+++ obj1/source/main.cpp
@@ -8,12 +8,29 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main() 
 {
-	
+	int i, j;
 
+	for(i = 0; i < 4; i++) {
+		for(j = 0; j <= 2-i; j++){
+			cout << " ";
+		}
+		for(j = 0; j <= 2*i; j++){
+			cout << (char)('A'+i);
+		}
+		cout << endl;
+	}
 
-
+	for(i = 0; i < 3; i++){
+		for(j = 0; j <= i; j++){
+			cout << " ";
+		}
+		for(j = 0; j<= 4-2*i; j++){
+			cout << "*";
+		}
+		cout << endl;
+	}
 
 
 
